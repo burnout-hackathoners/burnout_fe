dd52af8679cbcb7d25edf43dd23cec1b51aae702
Oleksandr Kharchenko <oleksandr.k@fundomate.com>
2023-11-04 17:03:30 +0200

    fixed type

--- src/services/api/httpProvider.ts
+++ src/services/api/httpProvider.ts
@@ -1,30 +1,14 @@
-import { AxiosResponse, AxiosRequestConfig, AxiosError, AxiosInstance } from 'axios';
-
-export interface HttpError extends AxiosError {
-    [x: string]: any;
-}
+import { AxiosResponse, AxiosRequestConfig, AxiosInstance } from 'axios';
 
 export const httpProvider = (client: AxiosInstance) => {
     const get = function <T>(uri: string, config?: AxiosRequestConfig): Promise<T> {
         return client
             .get(uri, config)
             .then((res: AxiosResponse) => res?.data)
-            .catch((e: HttpError) => {
+            .catch((e) => {
                 throw e;
             });
     };
 
-    const head = (uri: string, config?: AxiosRequestConfig): Promise<AxiosResponse> => {
-        return client
-            .head(uri, config)
-            .then((res: AxiosResponse) => res)
-            .catch((e: HttpError) => {
-                throw e;
-            });
-    };
-
-    return {
-        get,
-        head,
-    };
+    return { get };
 };
