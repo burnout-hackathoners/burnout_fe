aeb7e4027305ebf60d69f290a4551d1088c02de4
Oleksandr Kharchenko <oleksandr.k@fundomate.com>
2023-11-05 09:14:17 +0200

    fixed styles and sort data

--- src/components/UserBadgesWidget/index.tsx
+++ src/components/UserBadgesWidget/index.tsx
@@ -15,45 +15,34 @@ function UserBadgesWidget() {
   if (isError) return <Errors />;
   if (!achievements) return <NoData />;
 
-  const completedCurriculums = achievements.data.filter(
-    (curriculum) => curriculum.completion_percentage === 100,
+  const sortedCurriculums = achievements.data.filter(
+    (curriculum) => curriculum.completion_percentage > 0,
   );
-  const notCompletedCurriculums = achievements.data.filter(
-    (curriculum) =>
-      curriculum.completion_percentage !== 100 &&
-      curriculum.completion_percentage > 0,
+  sortedCurriculums.sort(
+    (a, b) => b.completion_percentage - a.completion_percentage,
   );
 
   return (
     <section className="CollapsibleSurface__CollapsibleSection-sc-78c0a714-5 fwjoFK">
       <div className="badges-wrapper CollapsibleSurface__IconWrapper-sc-78c0a714-3 idroUI">
-        {[...completedCurriculums!, ...notCompletedCurriculums!].map(
-          (curriculum) => (
-            <>
-              {curriculum.completion_percentage > 0 && (
-                <a
-                  href={`/qa-team/curriculums/${curriculum.id}`}
-                  key={curriculum.id}
-                >
-                  <Tooltip
-                    id={`tooltip-${curriculum.id}`}
-                    place="top"
-                    content={curriculum.title}
-                  />
-                  <div
-                    data-tooltip-id={`tooltip-${curriculum.id}`}
-                    className="FlexContainer-sc-383a0367-0 Avatar__CompletionStatusAlignmentWrapper-sc-3c5e4c94-5 heWXvb dnhoxd"
-                  >
-                    <BarChart
-                      completionPercentage={curriculum.completion_percentage}
-                    />
-                    <Avatar icon={curriculum.icon} />
-                  </div>
-                </a>
-              )}
-            </>
-          ),
-        )}
+        {sortedCurriculums.map((curriculum) => (
+          <a href={`/qa-team/curriculums/${curriculum.id}`} key={curriculum.id}>
+            <Tooltip
+              id={`tooltip-${curriculum.id}`}
+              place="top"
+              content={curriculum.title}
+            />
+            <div
+              data-tooltip-id={`tooltip-${curriculum.id}`}
+              className="FlexContainer-sc-383a0367-0 Avatar__CompletionStatusAlignmentWrapper-sc-3c5e4c94-5 heWXvb dnhoxd"
+            >
+              <BarChart
+                completionPercentage={curriculum.completion_percentage}
+              />
+              <Avatar icon={curriculum.icon} />
+            </div>
+          </a>
+        ))}
       </div>
     </section>
   );
